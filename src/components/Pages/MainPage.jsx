5b29f57003e40c72a79f500754b1e91783272f52
EldarSalyamov99 <eldarsalamov99@gmail.com>
2023-08-30 22:09:48 +0300

    Подкрутил ПНГ

--- src/components/Pages/MainPage.jsx
+++ src/components/Pages/MainPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
+
 export default function MainPage() {
   return (
+    <>
     <h1>Главная страница</h1>
+    <div className='test3'>
+      <div className='test'>
+        <div className='test2' />
+      </div>
+    </div>
+    </>
+
+
   )
 }
